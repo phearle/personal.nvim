76ca3d3b6526926c968842d7ac467f027a089e21
Peter H. Earle <95703+phearle@users.noreply.github.com>
2024-01-04 15:50:37 -0400

    feat: added gitsigns and lazygit plugins.

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,4 +1,22 @@
 return {
     {
+        'lewis6991/gitsigns.nvim',
+        config = function ()
+            require('gitsigns').setup()
+        end,
+    },
+    {
+        'kdheepak/lazygit.nvim',
+        keys = function ()
+            require('which-key').register({
+                ['<leader>g'] = { name = 'Lazy [G]it', _ = 'which_key_ignore' },
+            })
+            return {
+                { '<leader>gg', '<cmd>:LazyGit<cr>', desc = '[G]it UI' },
+            }
+        end,
+        dependencies = {
+            'nvim-lua/plenary.nvim'
+        },
     }
 }
