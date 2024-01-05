0cc11351e6c576c06a76cd96989b8d80aedf7ddd
Peter H. Earle <95703+phearle@users.noreply.github.com>
2024-01-04 20:46:33 -0400

    chore: disabled lazy change detection

--- init.lua
+++ init.lua
@@ -14,7 +14,10 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup({
+    spec = 'plugins',
+    change_detection = { notify = false }
+})
 
 vim.o.expandtab = true
 vim.o.tabstop = 4
