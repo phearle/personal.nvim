4c5d0f7cc7f6cdc78fe067d627f3ae80814e7711
Peter H. Earle <95703+phearle@users.noreply.github.com>
2024-01-05 20:01:00 -0400

    feat: added more languages to treesitter ensure_installed.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,7 +8,7 @@ return {
         config = function()
             require('nvim-treesitter.configs').setup({
                 ensure_installed = {
-                    'c', 'cpp', 'go', 'lua', 'python', 'rust'
+                    'c', 'cpp', 'go', 'lua', 'python', 'rust', 'javascript', 'typescript', 'bash'
                 },
             })
         end,
