e1140c2514af4ce8b3a236d269502820ce366d59
Peter H. Earle <95703+phearle@users.noreply.github.com>
2024-01-06 18:40:12 -0400

    chore: slight tweak in lsp config.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -46,9 +46,10 @@ local on_attach = function(_, bufnr)
     -- end, '[W]orkspace [L]ist Folders')
 
     -- Create a command `:Format` local to the LSP buffer
-    vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+    local format = function(_)
         vim.lsp.buf.format()
-    end, { desc = 'Format current buffer with LSP' })
+    end
+    vim.api.nvim_buf_create_user_command(bufnr, 'Format', format, { desc = 'Format current buffer with LSP' })
 end
 
 return {
