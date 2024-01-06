fa48a925d8b21b20fb833fdc87db6c34054db096
Peter H. Earle <95703+phearle@users.noreply.github.com>
2024-01-06 15:28:10 -0400

    feat: added templ support with working treesitter config. updated some plugins.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -10,8 +10,18 @@ return {
                 ensure_installed = {
                     'c', 'cpp', 'go', 'lua', 'python', 'rust',
                     'javascript', 'typescript', 'bash', 'markdown',
-                    'markdown_inline', 'regex',
+                    'markdown_inline', 'regex', 'templ'
                 },
+                sync_install = false,
+                highlight = {
+                    enable = true,
+                },
+                additional_vim_regex_highlighting = false,
+            })
+            vim.filetype.add({
+                extension = {
+                    templ = 'templ',
+                }
             })
         end,
     },
