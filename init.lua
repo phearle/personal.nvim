9e3db8a60cafb1bc603b289ef22b5841936b755a
Peter H. Earle <95703+phearle@users.noreply.github.com>
2024-01-06 16:01:30 -0400

    feat: added mapping for moving lines up and down in v mode.

--- init.lua
+++ init.lua
@@ -19,6 +19,10 @@ require("lazy").setup({
     change_detection = { notify = false }
 })
 
+-- Move selected text up or down
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv-gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv-gv")
+
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
     callback = function()
