0f3d2c712b4c006c763aa3b84d62342350122e02
Peter H. Earle <95703+phearle@users.noreply.github.com>
2024-01-05 21:27:23 -0400

    feat: added zen mode and twilight.

---
+++ lua/plugins/zen.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        'folke/zen-mode.nvim',
+        opts = {
+            window = {
+                width = .9
+            }
+        },
+    },
+    {
+        'folke/twilight.nvim',
+    }
+}
