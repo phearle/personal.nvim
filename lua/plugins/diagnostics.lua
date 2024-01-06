10a40913d1e169561e2dca0ab361894eb2bc4b52
Peter H. Earle <95703+phearle@users.noreply.github.com>
2024-01-05 23:28:48 -0400

    feat: improved trouble integration with more mappings

--- lua/plugins/diagnostics.lua
+++ lua/plugins/diagnostics.lua
@@ -8,17 +8,46 @@ return {
         local toggle = function()
             trouble.toggle()
         end
+        local open = function()
+            trouble.open()
+        end
         local lsp_references = function()
             trouble.toggle('lsp_references')
+        end
+        local workspace_diag = function()
+            trouble.toggle('workspace_diagnostics')
+        end
+        local document_diag = function()
+            trouble.toggle('document_diagnostics')
+        end
+        local quickfix = function()
+            trouble.toggle('quickfix')
+        end
+        local loclist = function()
+            trouble.toggle('loclist')
+        end
+        local next = function()
+            trouble.next({ skip_groups = true, jump = true })
+        end
+        local prev = function()
+            trouble.previous({ skip_groups = true, jump = true })
         end
         require('which-key').register({
             ['<leader>x'] = { name = 'Trouble', _ = 'which_key_ignore' },
         })
         return {
-            { '<leader>xx', toggle,         desc = 'Trouble' },
-            { '<leader>xr', lsp_references, desc = 'LSP References' },
+            { '<leader>xx', toggle,         desc = 'Trouble Toggle' },
+            { '<leader>xw', workspace_diag, desc = 'Trouble Toggle Workspace Diagnostics' },
+            { '<leader>xd', document_diag,  desc = 'TroubleToggle Document Diagnostics' },
+            { '<leader>xq', quickfix,       desc = 'TroubleToggle Quickfix' },
+            { '<leader>xl', loclist,        desc = 'TroubleToggle Loclist' },
+            { '<leader>xo', open,           desc = 'Trouble Open' },
+            { '<leader>xn', next,           desc = 'Trouble Next Item' },
+            { '<leader>xp', prev,           desc = 'Trouble Previous Item' },
+            { '<leader>xr', lsp_references, desc = 'Trouble LSP References' },
         }
     end,
+
     config = function()
         require('trouble').setup()
     end
