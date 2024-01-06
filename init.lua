29e4f7b05dff5cca28a733e5a335bcd14cbd0708
Peter H. Earle <95703+phearle@users.noreply.github.com>
2024-01-05 20:01:25 -0400

    feat: added highlight on yank

--- init.lua
+++ init.lua
@@ -19,6 +19,14 @@ require("lazy").setup({
     change_detection = { notify = false }
 })
 
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+    group = highlight_group,
+    pattern = '*',
+})
 vim.o.expandtab = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 0
