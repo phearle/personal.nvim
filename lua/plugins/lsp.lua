63467afdd7584b2b38201e5bf9a73555603c36d6
Peter H. Earle <95703+phearle@users.noreply.github.com>
2024-01-06 23:51:05 -0400

    fix: changed conflicting keybinds for telescope lsp builtins.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -27,7 +27,7 @@ local on_attach = function(_, bufnr)
     nmap('<leader>la', vim.lsp.buf.code_action, 'Code Action')
 
     nmap('<leader>ld', require('telescope.builtin').lsp_definitions, 'Goto Definition')
-    nmap('<leader>lr', require('telescope.builtin').lsp_references, 'Goto References')
+    nmap('<leader>lR', require('telescope.builtin').lsp_references, 'Goto References')
     nmap('<leader>lI', require('telescope.builtin').lsp_implementations, 'Goto Implementation')
     nmap('<leader>lD', require('telescope.builtin').lsp_type_definitions, 'Type Definition')
     nmap('<leader>lsd', require('telescope.builtin').lsp_document_symbols, 'Document Symbols')
